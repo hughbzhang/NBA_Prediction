e10e2774d4506929321ecec93edf6dae23ada79a
Hugh Zhang <hughzhang@gmail.com>
2016-03-01 21:35:38 -0800

    save

--- parser.sh
+++ parser.sh
@@ -2,6 +2,7 @@
 # This is my parser for the basketball data gathered from landofbasketball.com
 cp $1 cur.txt
 
+
 sed -i 's/,//g' cur.txt # Remove all the commas
 sed -i 's/[0-9]*OT//g' cur.txt # Remove all the overtime statements
 sed -ie '/.\{20\}/!d' cur.txt # Remove all lines less than 20 chars long (dates)
@@ -11,5 +12,7 @@ sed -i 's/\([a-z]\)\s\+\([a-z]\)/\1\2/g' cur.txt # Make team names one word
 sed -i 's/\s\+/ /g' cur.txt # Remove extra spaces
 sed -i 's/originally.*//g' cur.txt # Edge case for rescheduled games
 sed -i 's/fulllist.*/playoffs/g' cur.txt # Edge case marking the beginning of playoffs
+
+mv cur.txt ${1:0:4}".txt"
 
 # At the end, you should get a list of WINNER WSCORE LOSER LSCORE LOCATION
